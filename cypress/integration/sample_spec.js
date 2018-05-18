0416c350dc72a4bd3495e0932fcaacb287b25cd5
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2018-05-18 14:49:55 +0100

    Write a second test using cy.visit

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,13 @@
-describe('myFirstTest', () => {
+describe('my first test', () => {
   it('does not do much', () => {
     expect(true).to.equal(true);
+  });
+});
+
+const myApp = 'http://localhost:3000/';
+
+describe('my next test', () => {
+  it('visits my app', () => {
+    cy.visit(myApp)
   });
 });
